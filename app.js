444a88cb547487724424c6f83188138471b5c26b
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-06 21:48:11 -0800

    clean up to js and move start button

--- app.js
+++ app.js
@@ -1,62 +1,96 @@
 $(document).ready(function() {
+
   console.log("js loaded!");
 
 
-
+// --> song code here
 function playOnLoad () {
   let playing = true;
-
   $("#theme-song").get(0).play();
-
   $("#stop").on("click", function () {
     console.log("stopper");
-  if (playing === true) {
-    $("#theme-song").get(0).pause();
-    return playing = false;
-  } else if (playOnLoad !== true) {
-    $("#theme-song").get(0).play();
-    return playing = true;
-  }});
-
+    if (playing === true) {
+      $("#theme-song").get(0).pause();
+      return playing = false;
+    } else if (playOnLoad !== true) {
+      $("#theme-song").get(0).play();
+      return playing = true;
+    }
+  });
 }
-
 playOnLoad();
+//--> end song code
+
 
 
 // $('#x-wing').animate("slow","linear");
 
 let isStart = false;
+
 $(".start").on("click", function() {
   if (isStart === false) {
-
-$(document).bind('keydown',function(pressA){
-    if(pressA.keyCode === 65) {
-      $('#darth-maul').animate({marginLeft: "+=100px"});
-        if ($("#darth-maul").css("marginLeft") === ("1300px")) {
+    $(document).bind('keydown',function(pressA){
+      if(pressA.keyCode === 65) {
+        $('#darth-maul').animate({marginLeft: "+=100px"});
+         if ($("#darth-maul").css("marginLeft") === ("1300px")) {
           $(".vaderdiv").css({display: "block"});
         }
-
-
-
-    } else if (pressA.keyCode === 76) {
-      $('#obi-wan').animate({marginLeft: "+=100px"});
+      } else if (pressA.keyCode === 76) {
+        $('#obi-wan').animate({marginLeft: "+=100px"});
         if ($('#obi-wan').css("marginLeft") === ("1300px")) {
           $('.yodadiv').css({display: "block"});
         }
-  }  
+      }
+    });
+  }
 });
-}});
 
-// $("body").onload(function() {
-  
+
+
+
+
+
+
+// !*!*!*!*!*!*!
+//
+// --> another way to write it player movements -->
+//
+// $(document).bind('keydown',function(pressA) {
+//     if(pressA.keyCode === 65) {
+//       $('#darth-maul').animate({marginLeft: "+=100px"});
+//     };
+//       if ($("#darth-maul").css("marginLeft") === ("1300px")) {
+//         $(".vaderdiv").css({display: "block"});
+//         isStart !== false;
+//       }
+//     });
+//
+//     $(document).bind('keydown',function(pressL) {
+//       if (pressL.keyCode === 76) {
+//        $('#obi-wan').animate({marginLeft: "+=100px"});
+//      };
+//        if ($('#obi-wan').css("marginLeft") === ("1300px")) {
+//         $('.yodadiv').css({display: "block"});
+//           isStart !== false;
+//         }
+//       });
+//   }
 // });
+// !*!*!*!*!*!*!
+
+
+
+// --> x-wing animation -->
 
 $(".start").on("click", function() {
-  $("#x-wing").animate({marginRight: "1800px"});
-  $("#x-wing").fadeIn();
+  // $("#x-wing").animate({marginRight: "1800px"}); 
+// while (badLoop === true) {
+$("#x-wing").fadeIn();
   $("#x-wing").animate({"margin-left":"1500px"}, 3000, "swing", function() {
-      $("#x-wing").animate({"margin-left":"0px"}, 2000, "swing", function(){
-              console.log("avengers assemble");})})});
+    $("#x-wing").fadeOut();
+  });
+});
+
 
 
 
